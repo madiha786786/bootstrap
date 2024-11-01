8eb3e4363fa651c67e187a6d990909b4040e9b0c
madiha786786 <madihafathima090@gmail.com>
2024-11-01 17:37:41 +0530

    Initial commit

---
+++ newf.html/file1.js
@@ -0,0 +1,14 @@
+// var a=10;
+// console.log(a);
+// console.log(number);
+// let number=10;
+let city="rampur";
+console.log(city);
+{
+    let city='rampur';
+    city='kolkata';
+    console.log(city);
+}
+console.log(city);
+console.log(city);
+ 
